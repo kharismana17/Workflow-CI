9ebbf433d1ec9141292f477f5d5d13e760dd33d9
kharismana17 <kharismana17@gmail.com>
2026-06-04 11:40:10 +0700

    fix workflow path

--- MLProject/Membangun_model/modelling.py
+++ MLProject/Membangun_model/modelling.py
@@ -5,22 +5,23 @@ import mlflow.sklearn
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
 
 mlflow.set_tracking_uri("file:./mlruns")
-
 mlflow.sklearn.autolog()
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))
+)
 
-file_path = os.path.join(
+DATA_PATH = os.path.join(
     BASE_DIR,
-    "Eksperimen_SML_Kharisma",
     "dataset_preprocessing",
     "stress_clean.csv"
 )
 
-df = pd.read_csv(file_path)
+print("Dataset:", DATA_PATH)
+
+df = pd.read_csv(DATA_PATH)
 
 X = df.drop("Stress_Level", axis=1)
 y = df["Stress_Level"]
@@ -33,14 +34,5 @@ X_train, X_test, y_train, y_test = train_test_split(
     stratify=y
 )
 
-model = RandomForestClassifier(
-    random_state=42
-)
-
+model = RandomForestClassifier(random_state=42)
 model.fit(X_train, y_train)
-
-y_pred = model.predict(X_test)
-
-accuracy = accuracy_score(y_test, y_pred)
-
-print(f"Accuracy: {accuracy:.4f}")
