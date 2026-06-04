6fb49346425d4bc22715989579cb924a52744ada
kharismana17 <kharismana17@gmail.com>
2026-06-04 14:49:19 +0700

    up

--- MLProject/Membangun_model/modelling.py
+++ MLProject/Membangun_model/modelling.py
@@ -1,25 +1,23 @@
 import os
 import pandas as pd
 import mlflow
-import mlflow.sklearn
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 mlflow.set_tracking_uri("file:./mlruns")
-mlflow.sklearn.autolog()
+mlflow.autolog()
 
 BASE_DIR = os.path.dirname(
-    os.path.dirname(os.path.abspath(__file__))
+os.path.dirname(os.path.abspath(__file__))
 )
 
 DATA_PATH = os.path.join(
-    BASE_DIR,
-    "dataset_preprocessing",
-    "stress_clean.csv"
+BASE_DIR,
+"dataset_preprocessing",
+"stress_clean.csv"
 )
-
-print("Dataset:", DATA_PATH)
 
 df = pd.read_csv(DATA_PATH)
 
@@ -27,12 +25,19 @@ X = df.drop("Stress_Level", axis=1)
 y = df["Stress_Level"]
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y,
-    test_size=0.2,
-    random_state=42,
-    stratify=y
+X,
+y,
+test_size=0.2,
+random_state=42,
+stratify=y
 )
 
 model = RandomForestClassifier(random_state=42)
+
 model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Accuracy:", accuracy)
