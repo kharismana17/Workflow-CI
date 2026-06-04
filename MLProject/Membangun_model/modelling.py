7d1f52d9f950de9d0aae965d2e7a826a92d536c0
kharismana17 <kharismana17@gmail.com>
2026-06-04 17:14:36 +0700

    up

--- MLProject/Membangun_model/modelling.py
+++ MLProject/Membangun_model/modelling.py
@@ -6,19 +6,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
-mlflow.set_experiment("Student_Stress_Classification")
+tracking_uri = os.getenv("MLFLOW_TRACKING_URI")
+
+if tracking_uri:
+    mlflow.set_tracking_uri(tracking_uri)
+
+mlflow.set_experiment("StressPrediction_Basic")
 
 mlflow.autolog()
 
 BASE_DIR = os.path.dirname(
-os.path.dirname(os.path.abspath(__file__))
+    os.path.dirname(os.path.abspath(__file__))
 )
 
 DATA_PATH = os.path.join(
-BASE_DIR,
-"dataset_preprocessing",
-"stress_clean.csv"
+    BASE_DIR,
+    "dataset_preprocessing",
+    "stress_clean.csv"
 )
 
 df = pd.read_csv(DATA_PATH)
@@ -27,11 +31,11 @@ X = df.drop("Stress_Level", axis=1)
 y = df["Stress_Level"]
 
 X_train, X_test, y_train, y_test = train_test_split(
-X,
-y,
-test_size=0.2,
-random_state=42,
-stratify=y
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
 )
 
 model = RandomForestClassifier(random_state=42)
@@ -42,4 +46,4 @@ y_pred = model.predict(X_test)
 
 accuracy = accuracy_score(y_test, y_pred)
 
-print("Accuracy:", accuracy)
+print(f"Accuracy: {accuracy:.4f}")
