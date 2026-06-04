b11503c512bdb5e51d1c6038aac01face5307ccc
kharismana17 <kharismana17@gmail.com>
2026-06-04 16:48:16 +0700

    up

--- MLProject/Membangun_model/modelling.py
+++ MLProject/Membangun_model/modelling.py
@@ -6,6 +6,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("Student_Stress_Classification")
+
 mlflow.autolog()
 
 BASE_DIR = os.path.dirname(
